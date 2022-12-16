d2abca43468ef2574a68477bf11e8894919113b2
yashgondkar <99051589+yashgondkar@users.noreply.github.com>
2022-12-16 16:45:43 -0500

    Delete storagetls.tf

--- storagetls.tf
+++
@@ -1,7 +0,0 @@
-resource "azurerm_storage_account" "example" {
-  name                     = "pactestsa1"
-  resource_group_name      = "var.rg_name"
-  location                 = "var.location"
-  account_tier             = "Standard"
-  account_replication_type = "GRS"
-}
